60baf47616536eccb15443aa94de5ffdf284c19b
Jerad Gallinger <jerad@jeradgallinger.ca>
2016-01-26 17:35:13 -0500

    Tweaks to create; bumped Ceibo version

--- test-support/page-object/create.js
+++ test-support/page-object/create.js
@@ -1,37 +1,47 @@
 import Ceibo from 'ceibo';
 
 function buildDescriptorWithContext(context) {
-  // Cribbed from Ceibo
   return function(treeBuilder, target, key, value) {
+    // Call the usual descriptor builder
     Ceibo.defaults.builder.descriptor(treeBuilder, target, key, value);
 
-    if (!target.context) {
+    if (context && !target.context) {
       target.context = context;
     }
   }
 }
 
 function buildObjectWithContext(context) {
-  // Cribbed from Ceibo
   return function(treeBuilder, target, key, value) {
-    if (!target.context) {
+    if (context && !target.context) {
       target.context = context;
     }
 
     if (key !== 'context') {
-      if (!value.context) {
+      if (context && !value.context) {
         value.context = context;
       }
 
+      // Call the usual object builder
       Ceibo.defaults.builder.object(treeBuilder, target, key, value);
     }
   };
 }
 
+// `definition` can include a key `context`, which is an
+// optional integration test `this` context.
+//
+// If a context is passed, it is used by actions, queries, etc.,
+// as the `this` in `this.$()`.
+//
+// If no context is passed, the global Ember acceptence test
+// helpers are used.
 export function create(definition, options = {}) {
   let context;
 
   if (definition && definition.context) {
+    // If a test context is passed in the definition,
+    // use it as the context
     context = definition.context;
   } else if (options && options.parent && options.parent.context) {
     // If `create()` is called by a collection, the context will
